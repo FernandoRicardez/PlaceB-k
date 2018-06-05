bbaec441532a32a87579ab751b0586d58738e818
Fernando Ricárdez <fernando.ricardez@hotmail.com>
2018-06-04 20:05:48 -0500

    firebase2

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,17 +1,17 @@
 
-  var config = {
-    apiKey: "AIzaSyBVHEVDmN4ZkOrS8ENz_1Z6X6txnRFfp5g",
-    authDomain: "place-bk.firebaseapp.com",
-    databaseURL: "https://place-bk.firebaseio.com",
-    projectId: "place-bk",
-    storageBucket: "place-bk.appspot.com",
-    messagingSenderId: "260965813880"
-  };
-  firebase.initializeApp(config);
+var config = {
+  apiKey: "AIzaSyBVHEVDmN4ZkOrS8ENz_1Z6X6txnRFfp5g",
+  authDomain: "place-bk.firebaseapp.com",
+  databaseURL: "https://place-bk.firebaseio.com",
+  projectId: "place-bk",
+  storageBucket: "place-bk.appspot.com",
+  messagingSenderId: "260965813880"
+};
+firebase.initializeApp(config);
 
 var ref = firebase.database();
 
-  
+
 
 var mapStyle = [
   {
@@ -49,57 +49,63 @@ var mapStyle = [
 var iconSelected = "food";
 
 function initMap() {
-    var laSalleBajio = {lat: 21.150908, lng: -101.71110470000002};
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 18,
-      center: laSalleBajio,
-      styles : mapStyle
-    });
-    var marker = new google.maps.Marker({
-      position: laSalleBajio,
-      map: map,
-      icon: icons['food'].icon
-    });
-
-
-       google.maps.event.addListener(map, 'click', function(event) {
-      var lat = event.latLng.lat(); 
-      var lng = event.latLng.lng(); 
-      
-      var markerSpot = {lat: lat, lng: lng};
-      var marker = new google.maps.Marker({
-        position: markerSpot,
-        map: map,
-        icon: icons[iconSelected].icon
-      });
-    }); 
-    
-  }
-
-  
-function insert()
-{
-  firebase.database().ref('users/').set({
-    username: "name",
-    email: "email",
-    profile_picture : "imageUrl"
+  var laSalleBajio = { lat: 21.150908, lng: -101.71110470000002 };
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 18,
+    center: laSalleBajio,
+    styles: mapStyle
   });
-}  
+  var marker = new google.maps.Marker({
+    position: laSalleBajio,
+    map: map,
+    icon: icons['food'].icon
+  });
 
-  var iconBase = 'images/smallIcons/';
-        var icons = {
-          food: {
-            icon: iconBase + 'restaurant.png'
-          },
-          sport: {
-            icon: iconBase + 'sport.png'
-          },
-          info: {
-            icon: iconBase + 'info-i_maps.png'
-          }
-        };
- 
-$(".iconPlaces-bar").on("click", function() {
+
+  google.maps.event.addListener(map, 'click', function (event) {
+    var lat = event.latLng.lat();
+    var lng = event.latLng.lng();
+
+    var markerSpot = { lat: lat, lng: lng };
+    var marker = new google.maps.Marker({
+      position: markerSpot,
+      map: map,
+      icon: icons[iconSelected].icon
+    });
+
+    firebase.database().ref('markers/').push({
+      position: markerSpot,
+     
+      icon: icons[iconSelected].icon
+    });
+
+  });
+
+}
+
+
+function insert() {
+  firebase.database().ref('users/').push({
+    username: "name2",
+    email: "email",
+    profile_picture: "imageUrl"
+  });
+}
+
+var iconBase = 'images/smallIcons/';
+var icons = {
+  food: {
+    icon: iconBase + 'restaurant.png'
+  },
+  sport: {
+    icon: iconBase + 'sport.png'
+  },
+  info: {
+    icon: iconBase + 'info-i_maps.png'
+  }
+};
+
+$(".iconPlaces-bar").on("click", function () {
   iconSelected = $(this).attr("alt");
 
   $(".iconPlaces-bar").css("background", "white");
